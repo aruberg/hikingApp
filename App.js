6990b336a466396dbf7040dfd9502234a7fb8b3b
jane548 <anmyers@ualberta.ca>
2020-10-08 08:57:28 -0700

    Installed react navigation

--- App.js
+++ App.js
@@ -1,3 +1,15 @@
-import react from 'react'
+import 'react-native-gesture-handler';
+import * as React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+
+const App = () => {
+  return (
+    // Wrap code with navigation container
+    <NavigationContainer>
+
+    </NavigationContainer>
+
+  );
+};
 
 export default app
