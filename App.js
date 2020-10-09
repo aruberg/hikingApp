534d5d05ce8ce038c9405df56cd9e4266483758f
jane548 <anmyers@ualberta.ca>
2020-10-09 14:58:08 -0700

    Basic splash screen completed

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import * as React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { ScreenStackHeaderCenterView } from 'react-native-screens';
-import SplashScreen from './Screens/SplashScreen';
+import SplashScreen from './screens/SplashScreen';
 
 const Stack = createStackNavigator();
 
@@ -11,7 +11,10 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="SplashScreen" component={SplashScreen} />
+        <Stack.Screen 
+          name="SplashScreen"
+          component={SplashScreen}
+          options={{headerShown: false}} />
       </Stack.Navigator>
     </NavigationContainer>
 
---
+++ screens/SplashScreen.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import { View, Image, Text, StyleSheet, Animated } from 'react-native';
+import logo from '../images/logo.png';
+
+class SplashScreen extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image source={logo} style={styles.image} />
+            </View>
+        )
+    }
+}
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#C98F39',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    image: {
+        resizeMode: "contain",
+    },
+});
